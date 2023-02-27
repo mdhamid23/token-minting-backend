5f2e2312bb6193b36c19a32b60e9ca8976356f9e
mdhamid23 <hamid@deepchainlabs.com>
2023-02-27 16:35:16 +0600

    create token updated

--- src/modules/createToken/create-token.controller.ts
+++ src/modules/createToken/create-token.controller.ts
@@ -1,32 +1,39 @@
 import { QueryParserPipe } from "@/common/pipes";
 import {
-    Body, Controller, Get, Param, Patch, Post, Query
-  } from "@nestjs/common";
-  import { ApiTags } from "@nestjs/swagger";
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { TokenService } from "./create-token.service";
 import { CreateTokenDto } from "./dto/create-token.dto";
-  
-  @ApiTags("Token")
-  @Controller({ path: "tokens" })
-  export class TokenController {
-    constructor(private readonly tokenService: TokenService) {}
-  
-    @Get()
-    FindAll( @Query(new QueryParserPipe("MANY", [])) findOption) {
-        return this.tokenService.findAll(findOption); 
-    }
-    @Get('/one')
-    FindOne( @Query(new QueryParserPipe("MANY", [])) findOption) {
-        return this.tokenService.findOne(findOption); 
-    }
-    @Post()
-    CreateToken(@Body() dto: CreateTokenDto) {
-      return this.tokenService.CreateToken(dto);
-    }
-  
-    @Patch("/:id")
-    UnSubscribe(@Param("id") id:string, @Body() dto: CreateTokenDto) {
-      return this.tokenService.updateOne(+id,dto);
-    }
+
+@ApiTags("Token")
+@Controller({ path: "tokens" })
+export class TokenController {
+  constructor(private readonly tokenService: TokenService) {}
+
+  @Get()
+  FindAll(@Query(new QueryParserPipe("MANY", [])) findOption) {
+    return this.tokenService.findAll(findOption);
   }
-  
+  @Get("/one")
+  FindOne(@Query(new QueryParserPipe("ONE")) findOption) {
+    return this.tokenService.findOne(findOption);
+  }
+  @Post()
+  CreateToken(@Body() dto: CreateTokenDto) {
+    console.log({ dto });
+    // return { dto };
+    return this.tokenService.CreateToken(dto);
+  }
+
+  @Patch("/:id")
+  UnSubscribe(@Param("id") id: string, @Body() dto: CreateTokenDto) {
+    return this.tokenService.updateOne(+id, dto);
+  }
+}
